7c040da209edb4f92f139d675fad77c28125f580
Michael Farwell <whitewolf9909@gmail.com>
2023-06-28 15:00:28 -0400

    complete chapter 3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    user = {"username": "Michael"}
+    posts = [
+        {
+            "author": {"username": "Bilbo"},
+            "body": "I'm going on an adventure!"
+        },
+        {
+            "author": {"username": "Gandalf"},
+            "body": "I have no memory of this place..."
+        }
+    ]
+    return render_template("index.html",
+                           title="Home",
+                           user=user,
+                           posts=posts)
